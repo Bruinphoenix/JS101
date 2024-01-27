00669b105d489ca493ae26121e6f8990e4cb5805
ConorMcCaulley <conormccaulley@ConorMcCaulleys-MacBook-Air.local>
2024-01-27 15:19:34 +0700

    added code comments

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,12 +1,9 @@
-
-
-
-
 function calculator() {
+  //Import required objects and set internal variables
   const readline = require('readline-sync');
   const ALL_MESSAGES = require('./calc_messages.json');
-  //__________________________________________________________
-  //INTERNAL FUNCTION DEFINITIONS
+  let runAgain = true;
+
   //defines the prompt method used to communicate with the user
   function prompt(message) {
     console.log(`=> ${message}`);
@@ -16,41 +13,51 @@ function calculator() {
   function invalidNumber(number) {
     return number.trimStart() === '' || Number.isNaN(Number(number));
   }
-  //________________________________________________________________________________
+  //______________________________________________________________
 
   //FUNCTION BODY
+  //prompt user to decide the interface language
   prompt(ALL_MESSAGES.decideLanguage);
   let language = readline.question();
-  const MESSAGES = jsonObj[language];
 
+  //create an object storing the messages in the selected language
+  const MESSAGES = ALL_MESSAGES[language];
+
+  //welcome the user to the program
   prompt(MESSAGES.welcome);
-  let runAgain = true;
 
+  //promt the user for the first number they want to use
   while (runAgain) {
     prompt(MESSAGES.promt1);
     let number1 = readline.question();
 
+    //ensure the entered number is valid
     while (invalidNumber(number1)) {
       prompt(MESSAGES.invalidNum);
       number1 = readline.question();
     }
 
+    //promt the user for the first number they want to use
     prompt(MESSAGES.promt2);
     let number2 = readline.question();
 
+    //ensure the entered number is valid
     while (invalidNumber(number2)) {
       prompt(MESSAGES.invalidNum);
       number2 = readline.question();
     }
 
+    //promt the user for the operator they want to use;
     prompt(MESSAGES.promtOperator);
     let operation = readline.question();
 
+    //ensure the entered operator is valid
     while (!['1', '2', '3', '4'].includes(operation)) {
-      prompt(MESSAGES.invalidOperator)
+      prompt(MESSAGES.invalidOperator);
       operation = readline.question();
     }
 
+    //output the result of the users numbers and operator
     let output;
     switch (operation) {
       case '1':
@@ -67,17 +74,17 @@ function calculator() {
         break;
     }
 
+    //output the result
     prompt(`The result is: ${output}`);
 
-    //check if the use would like to run another calculation 
+    //check if the use would like to run another calculation
     while (true) {
       prompt(MESSAGES.promtRunAgain);
       let runAgainResponse = readline.question();
       if (runAgainResponse === 'n') {
         runAgain = false;
         break;
-      }
-      else if (runAgainResponse === 'y') {
+      } else if (runAgainResponse === 'y') {
         break;
       }
     }
